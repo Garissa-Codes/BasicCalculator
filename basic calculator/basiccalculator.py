a689f4a48eec3ba8f61ff0afda51c21eb5c5e8f5
Garissa-Codes <danielndeto222@gmail.com>
2024-11-24 09:27:54 -0800

    Added calculator program and README

---
+++ basic calculator/basiccalculator.py
@@ -0,0 +1,31 @@
+def calculator():
+    print("This is My Basic Calculator. Welcome!")
+    try:
+         # Get user input for two numbers and an operation
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = input("Enter the operation (+, -, *, /): ")
+
+       # Perform the operation based on user input
+        if operation == "+":
+            result = num1 + num2
+            print(f"{num1} + {num2} = {result}")
+        elif operation == "-":
+            result = num1 - num2
+            print(f"{num1} - {num2} = {result}")
+        elif operation == "*":
+            result = num1 * num2
+            print(f"{num1} * {num2} = {result}")
+        elif operation == "/":
+            if num2 != 0:
+                result = num1 / num2
+                print(f"{num1} / {num2} = {result}")
+            else:
+                print("Error: Division by zero is not allowed.")
+        else:
+            print("Invalid operation. Please use +, -, *, or /.")
+    except ValueError:
+        print("Invalid input. Please enter numeric values for the numbers.")
+
+# Run the calculator 
+calculator()
